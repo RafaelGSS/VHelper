268ca2a1ed762fa6703d66c117b643799ce7d311
rafaelgss <rafael.nunu@hotmail.com>
2018-12-04 23:48:40 -0200

    added spdlog include

--- VHelper/src/core/winapi/WinEventBase.cpp
+++ VHelper/src/core/winapi/WinEventBase.cpp
@@ -1,2 +1,35 @@
 #include "WinEventBase.h"
 
+#include <src/utils/spdlog/spdlog.h>
+
+
+void core::winapi::WinEventBase::run()
+{
+	if (!m_hook) {
+		spdlog::critical("Hook is not setted");
+		return;
+	}
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	spdlog::critical("Out of GetMessage");
+}
+
+void core::winapi::WinEventBase::shutdown()
+{
+	spdlog::debug("Shutdown the Hook");
+	UnhookWinEvent(m_hook);
+	CoUninitialize();
+}
+
+core::winapi::WinEventBase::WinEventBase()
+{
+}
+
+core::winapi::WinEventBase::~WinEventBase()
+{
+}
--- VHelper/src/core/winapi/WinEventBase.h
+++ VHelper/src/core/winapi/WinEventBase.h
@@ -1,11 +1,30 @@
 #pragma once
+#include <Windows.h>
+
 namespace core::winapi {
 
 	class WinEventBase
 	{
 	public:
+		virtual void run();
+		virtual void shutdown();
+		
 		WinEventBase();
-		~WinEventBase();
+		virtual ~WinEventBase();
+	protected:
+		virtual void boot() = 0;
+		virtual void __stdcall winEventProcCallback(
+			HWINEVENTHOOK hWinEventHook, 
+			DWORD dwEvent,
+			HWND hwnd, 
+			LONG idObject, 
+			LONG idChild, 
+			DWORD dwEventThread, 
+			DWORD dwmsEventTime
+		) = 0;
+		
+	private:
+		HWINEVENTHOOK m_hook;
 	};
 }
 
